2777c99974e0dce7f4c131a37ddb2e4981ad8471
amaljudelitto <amaljudelitto@gmail.com>
2025-06-19 16:54:17 +0530

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -2,3 +2,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 %pip install openai  langchain langchain-openai yfinance agno
+
+from google.colab import files
+uploaded = files.upload()  # Upload secret.env manually
+
