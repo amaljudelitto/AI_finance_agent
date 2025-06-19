d2389e4732973ed6ff37421eabe11a3167f6152e
amaljudelitto <amaljudelitto@gmail.com>
2025-06-19 17:23:39 +0530

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,7 +1,7 @@
 import warnings
 warnings.filterwarnings('ignore')
 
-%pip install openai  langchain langchain-openai yfinance agno
+%pip install openai  langchain langchain-openai yfinance agno streamlit
 
 
 
